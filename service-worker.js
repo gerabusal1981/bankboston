655a722c7fa88d10749b03052dae9382835fe387
gerabusal1981 <gerardo.salazar.bustos@gmail.com>
2025-08-03 19:57:47 -0400

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,44 +1,51 @@
-const CACHE_NAME = 'bankofboston-cache-v1';
+const CACHE_NAME = 'bankboston-v2';
 const urlsToCache = [
+  '/',
   'index.html',
   'account.html',
   'style.css',
   'app.js',
   'manifest.json',
-  'icons/icon-192.png',
-  'icons/icon-512.png',
-  'icons/header_logo.png',
-  'icons/bank_building.jpg'
+  'icons/icon-192x192.png',
+  'icons/icon-512x512.png'
 ];
 
-// Instalación: almacenar en caché los archivos esenciales
+// Instalación: cachear recursos estáticos
 self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll(urlsToCache);
-    })
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('Cache abierto');
+        return cache.addAll(urlsToCache);
+      })
   );
 });
 
-// Activación: limpiar cachés antiguas
-self.addEventListener('activate', event => {
-  event.waitUntil(
-    caches.keys().then(cacheNames =>
-      Promise.all(
-        cacheNames
-          .filter(name => name !== CACHE_NAME)
-          .map(name => caches.delete(name))
-      )
-    )
-  );
-});
-
-// Interceptar solicitudes de red
+// Estrategia: Cache First
 self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
+    caches.match(event.request)
+      .then(response => {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      })
   );
 });
 
+// Limpiar caches antiguos
+self.addEventListener('activate', event => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
